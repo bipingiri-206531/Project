2af1f50b43a876a69b0b88e65fbf8c073490c626
bipingiri-206531 <bipingiri206531@gmail.com>
2026-03-31 17:15:34 +0545

    This is spring  security class

--- com/practice2/practice2/repository/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.practice2.practice2.repository;
-
-import com.practice2.practice2.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
