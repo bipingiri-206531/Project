b836fe82f867fb19c02b9267021264e8395f78a7
bipingiri-206531 <bipingiri206531@gmail.com>
2026-03-31 17:57:55 +0545

    Delete java directory

--- java/java/com/practice2/practice2/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice2.practice2.repository;
-
-import com.practice2.practice2.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-
-}
--- java/java/com/practice2/practice2/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.practice2.practice2.repository;
-
-import com.practice2.practice2.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long>{
-
-}
